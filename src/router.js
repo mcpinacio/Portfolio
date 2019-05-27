08f933680f0abcc21c72fc71fdb3f1f899503d3d
Mariana Inácio <mcpinacio@gmail.com>
2019-05-27 09:49:50 +0100

    illustration view

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Recollections from './views/recollections.vue'
 import TOBE from './views/tobe.vue'
+import Illustration from './views/illustration.vue'
 
 Vue.use(Router)
 
@@ -25,15 +26,12 @@ export default new Router({
       name: 'tobe',
       component: TOBE
     },
-      // 
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
+    {
+      path: '/projects/illustration',
+      name: 'illustration',
+      component: Illustration
+    },
+
   ],
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
---
+++ src/views/illustration.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="container transition">
+		<div class="projects transition">
+			
+			<div class="title transition">
+				<h1 class="transition">Pop Culture</h1>
+			</div>
+
+			<div class="desc-container transition">
+			<div class="infos transition">
+
+				<div class="year transition">
+					<h3 class="margin-top-none transition">Year</h3>
+					<p class="year2 transition">2017<br>to present</p>
+				</div>
+			</div>
+
+			<div class="description transition">
+
+				<h2 class=" transition">Monochromatic portraits influenced by the colour field art movement.</h2>
+
+				<p class="transition">Inspired by colour field art movement, I allied its usage of big flat surfaces of solid colour to a high-contrast and vectoral art style.<br>One of the main challenges of creating high-contrast portraits is how to work the light and shadows without losing the distinguishable features that make each person portraited unique and recognisable.</p>
+			</div>
+		</div>
+		</div>
+
+		<div class="vertical transition">
+			<figure data-aos="fade-in"
+     				data-aos-delay="50"
+     				data-aos-duration="750"
+     				data-aos-easing="ease-in-out">
+				<img src="@/assets/portraits/walter_white.png" alt="Walter White Portrait" class="img-vertical transition">
+				<figcaption>Bryan Cranston as Walter White in <i>Breaking Bad</i></figcaption>
+			</figure>
+			<figure data-aos="fade-in"
+     				data-aos-delay="50"
+     				data-aos-duration="750">
+				<img src="@/assets/portraits/mindy_kalling.png" alt="poster with sun and trees" class="img-vertical transition">
+				<figcaption>Mindy Kalling</figcaption>
+			</figure>
+			<figure data-aos="fade-in"
+     				data-aos-delay="50"
+     				data-aos-duration="750">
+				<img src="@/assets/portraits/mariana_cabral.png" alt="poster with sun and trees" class="img-vertical transition">
+				<figcaption>Mariana Cabral</figcaption>
+			</figure>
+		</div>
+		<div class="projnav">
+			<router-link to="/projects/recollections">
+			<p class="prev">previous</p></router-link>
+			<router-link to="/projects/tobe"><p>next</p></router-link>
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss" src="./illustration.scss"/>
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"src="https://unpkg.com/aos@2.3.1/dist/aos.js">
+	AOS.init();
+</script>
+
+<script>
+	export default {
+	  name: 'illustration',
+	  props: {
+	  }
+	}
+</script>
