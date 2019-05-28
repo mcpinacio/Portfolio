13632a5bf5fa28631cbcb6a93ceecb4d892bfdb3
Mariana Inácio <mcpinacio@gmail.com>
2019-05-28 09:37:21 +0100

    contacts hover

--- src/components/contacts/contacts.vue
+++ src/components/contacts/contacts.vue
@@ -1,7 +1,7 @@
 <template>
 <section class="contacts">
 	 	<div class="text transition">
-	 	<h1 class="transition">Let's work together<br>Drop me an <a class="cl-effect-3" href="mailto:mcpinacio@gmail.com">email</a></h1>
+	 	<h1 class="transition">Let's work together<br>Drop me an email</h1>
 	 </div>
 
 	 	<div class="cl-effect-3 social transition">
