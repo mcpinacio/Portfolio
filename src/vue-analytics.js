f29527b469ce6f79039fefb5602179fc1545318e
Mariana Inácio <mcpinacio@gmail.com>
2019-08-03 16:22:14 +0100

    talknwalk page

---
+++ src/vue-analytics.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-144042790-1'
+})
+
+// Vue.use(VueAnalytics, {
+//   id: '',
+//   router,
+//   autoTracking: {
+//     screenview: true
+//   },
+// })
