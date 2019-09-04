a6e6872f1e07ab8c091511b64623455a174df036
Mariana Inácio <mcpinacio@gmail.com>
2019-09-04 18:18:00 +0100

    Mobile view
    Added mobile view to home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="home" class="home">
     <About/> 
-    <Projects id="projects"/>
+    <ProjectsMobile class="mobile" id="projects"/>
+    <Projects class="desktop" id="projects"/>
     <Contacts/>
   </div>
 </template>
 
+<style scoped lang="scss" src='./home.scss'/>
+
 <script>
 import Projects from '@/components/projects-section/projects-section.vue'
+import ProjectsMobile from '@/components/projects-section/projects_mobile.vue'
 import About from '@/components/about/about.vue'
 import Contacts from '@/components/contacts/contacts.vue'
 
@@ -16,6 +20,7 @@ export default {
   components: {
     Contacts,
     Projects,
+    ProjectsMobile,
     About
   }
 }
