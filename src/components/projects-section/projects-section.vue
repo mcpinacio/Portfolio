04de0d77f67788b893592887e2bd345e10bc77b3
Mariana Inácio <mcpinacio@gmail.com>
2019-09-04 18:16:38 +0100

    Project Sections
    Mobile and Destktop views

--- src/components/projects-section/projects-section.vue
+++ src/components/projects-section/projects-section.vue
@@ -1,104 +1,54 @@
 <template>
 	<section id="projects" class="projects-section">
 
-	<article  class="project_container tobe">
-		<router-link to="/projects/tobe"> 
-			<figure class="rectangle animate">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 251.825 251.825">
-					<g transform="translate(0 0)" fill="none" stroke="#10223c" stroke-width="4">
-						<rect width="251.825" height="251.825" stroke="none"/>
-						<rect x="2" y="2" width="247.825" height="247.825" fill="none"/>
-					</g>
-				</svg>
-			</figure>
-
-			<figure class="circle">
-				<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 234 234">
-					<circle id="tobe" cx="117" cy="117" r="117" fill="#EFBCC6"/>
-				</svg>
-			</figure>
-
-			<div class="title">
-				<h4>The Other<br>Body Experience</h4>
-				<h5>Interactive Art Installation</h5>
-			</div>
-		</router-link>
-	</article>
-
-	<article  class="project_container recollections">
-	<router-link to="/projects/recollections"> 
-		<figure class="rectangle animate">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 251.825 251.825">
-				<g transform="translate(0 0)" fill="none" stroke="#10223c" stroke-width="4">
-					<rect width="251.825" height="251.825" stroke="none"/>
-					<rect x="2" y="2" width="247.825" height="247.825" fill="none"/>
-				</g>
-			</svg>
-		</figure>
-
-		<figure class="circle">
-			<svg xmlns="http://www.w3.org/2000/svg" width="180" height="180">
-				<circle cx="90" cy="90" r="90" fill="#EFBCC6"/>
-			</svg>
-		</figure>
-
-		<div class="title">
-			<h4>Recollections</h4>
-			<h5>Game Design</h5>
+		<div class="images">	
+			<img :src="this.getImgSelected">
 		</div>
-	</router-link>
-	</article>
 
-	<article  class="project_container illustration">
-	<router-link to="/projects/illustration"> 
-		<figure class="rectangle animate">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 251.825 251.825">
-				<g transform="translate(0 0)" fill="none" stroke="#10223c" stroke-width="4">
-					<rect width="251.825" height="251.825" stroke="none"/>
-					<rect x="2" y="2" width="247.825" height="247.825" fill="none"/>
-				</g>
-			</svg>
-		</figure>
-
-		<figure class="circle">
-			<svg xmlns="http://www.w3.org/2000/svg" width="180" height="180">
-				<circle cx="90" cy="90" r="90" fill="#EFBCC6"/>
-			</svg>
-		</figure>
-
-		<div class="title">
-			<h4>Pop Culture</h4>
-			<h5>Illustration Project</h5>
-		</div>
-	</router-link>
-	</article>
-
-	<article  class="project_container talknwalk">
-	<router-link to="/projects/talknwalk"> 
-		<figure class="rectangle animate">
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 251.825 251.825">
-				<g transform="translate(0 0)" fill="none" stroke="#10223c" stroke-width="4">
-					<rect width="251.825" height="251.825" stroke="none"/>
-					<rect x="2" y="2" width="247.825" height="247.825" fill="none"/>
-				</g>
-			</svg>
-		</figure>
-
-		<figure class="circle">
-			<svg xmlns="http://www.w3.org/2000/svg" width="180" height="180">
-				<circle cx="90" cy="90" r="90" fill="#EFBCC6"/>
-			</svg>
-		</figure>
-
-		<div class="title">
-			<h4>TalknWalk</h4>
-			<h5>Mental Health App</h5>
-		</div>
-	</router-link>
-	</article>
-
+		<ul class="work">
+			<li class="group"
+				v-on:mouseover="itemSelected = 0"
+				v-on:mouseleave="itemSelected = -1">
+				<router-link to="/projects/talknwalk"> 
+					<h2>TalknWalk App</h2>
+					<h3>Produt Design</h3>
+				</router-link>
+			</li>
+			<li class="group" 				
+				v-on:mouseover="itemSelected = 1"
+				v-on:mouseleave="itemSelected = -1">
+				<router-link to="/projects/talknwalk_web">
+					<h2>TalknWalk Website</h2>
+					<h3>Web Design and Development</h3>
+				</router-link>
+			</li>
+			<li class="group"
+				v-on:mouseover="itemSelected = 2"
+				v-on:mouseleave="itemSelected = -1">
+				<router-link to="/projects/ipsslab">
+					<h2>IPSSLAB by Unitate</h2>
+					<h3>Visual Identity</h3>
+				</router-link>
+			</li>
+			<li class="group"
+				v-on:mouseover="itemSelected = 3"
+				v-on:mouseleave="itemSelected = -1">
+				<router-link to="/projects/illustration">
+					<h2>PopCulture</h2>
+					<h3>Vector Illustration</h3>
+				</router-link>
+				
+			</li>
+			<li class="group"
+				v-on:mouseover="itemSelected = 4"
+				v-on:mouseleave="itemSelected = -1">
+				<router-link to="/projects/recollections">
+					<h2>Recollections</h2>
+					<h3>Game Design</h3>
+				</router-link>
+			</li>
+		</ul>
 	</section>
-
 </template>
 
 <style scoped lang="scss" src='./projects-section.scss'/>
@@ -107,7 +57,34 @@
 export default {
   name: 'Projects',
   props: {
-  }
+  },
+
+	data() {
+		return{
+			itemSelected: -1,
+			images: [	
+				"talknwalk_app/0_preview.png",
+				"talknwalk_web/0_preview.png",
+				"ipsslab/0_preview.png",
+				"portraits/1_preview.png",
+				"recollections/1_preview.png"
+			]
+		}
+	},
+
+	methods: {
+	},
+
+	computed: {
+		getImgSelected(){
+			if (this.itemSelected === -1){
+				return "";
+			} else {
+				const img = this.images[this.itemSelected];
+				return `/images/${img}`;
+			}	
+		}
+	}
 }
 </script>
 
---
+++ src/components/projects-section/projects_mobile.vue
@@ -0,0 +1,75 @@
+<template>
+	<section id="projects" class="projects-section">
+		<div 	class="block"
+				data-aos="fade-up"
+				data-aos-duration="850"
+				data-aos-anchor-placement="center-bottom">
+			<router-link to="/projects/talknwalk"> 
+				<img src="images/talknwalk_app/0_preview.png" alt="talnkwalk app screens">
+				<div class="titles">
+					<h2>TalknWalk App</h2>
+					<h3>Product Design</h3>
+				</div>
+			</router-link>
+		</div>
+		<div 	class="block"
+				data-aos="fade-up"
+				data-aos-duration="850"
+				data-aos-anchor-placement="center-bottom">
+			<router-link to="/projects/talknwalk_web"> 
+				<img src="/images/talknwalk_web/0_preview.png" alt="talnkwalk website screen on laptop">
+				<div class="titles">
+					<h2>TalknWalk Website</h2>
+					<h3>Web Design and Development</h3>
+				</div>
+			</router-link>
+		</div>
+		<div 	class="block"
+				data-aos="fade-up"
+				data-aos-delay="25"
+				data-aos-duration="850"
+				data-aos-anchor-placement="center-bottom">
+			<router-link to="/projects/ipsslab"> 
+				<img src="images/ipsslab/0_preview.png" alt="ipss lab logo">
+				<div class="titles">
+					<h2>IPSSLAB by Unitate</h2>
+					<h3>Visual Identity</h3>
+				</div>
+			</router-link>
+		</div>
+		<div 	class="block"
+				data-aos="fade-up"
+				data-aos-delay="25"
+				data-aos-duration="850"
+				data-aos-anchor-placement="center-bottom">
+			<router-link to="/projects/illustration"> 
+				<img src="/images/portraits/0_preview.png" alt="Walter White Portrait illustration">
+				<div class="titles">
+					<h2>PopCulture</h2>
+					<h3>Vector Illustration</h3>
+				</div>
+			</router-link>
+		</div>
+		<div 	class="block"
+				data-aos="fade-up"
+				data-aos-delay="25"
+				data-aos-duration="850"
+				data-aos-anchor-placement="center-bottom">
+			<router-link to="/projects/recollections"> 
+				<img src="/images/recollections/1_preview.png" alt="ipss lab logo">
+				<div class="titles">
+					<h2>Recollections</h2>
+					<h3>Game Design</h3>
+				</div>
+			</router-link>
+		</div>
+	</section>
+</template>
+
+<style scoped lang="scss" src="./projects-section-mobile.scss"/>
+
+<script>
+	export default {
+		name: 'ProjectsMobile'
+	}
+</script>
